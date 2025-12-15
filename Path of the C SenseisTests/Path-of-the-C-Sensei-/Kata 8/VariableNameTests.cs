772a23d72438f9125a361d9bf4ad43aba5eccd9c
Daniel Rodolfo Rueda Gutierrez <74000847+Deart722@users.noreply.github.com>
2025-12-15 20:29:29 +0100

    #Kata 8: VariableName

---
+++ Path of the C SenseisTests/Path-of-the-C-Sensei-/Kata 8/VariableNameTests.cs
@@ -0,0 +1,42 @@
+﻿using Path_of_the_C_Senseis.Path_of_the_C_Sensei_.Kata8;
+using System;
+using NUnit.Framework;
+
+
+
+namespace Path_of_the_C_Senseis.Path_of_the_C_Sensei_.Kata8.Tests
+{
+    [TestFixture]
+    public class VariableNameExampleTests
+    {
+        [Test, Order(1)]
+        public void TestBoolVariable()
+        {
+            VariableName variables = new VariableName();
+            Assert.That(CheckType(variables, "bool", typeof(bool)), Is.True);
+        }
+
+        [Test, Order(2)]
+        public void TestCharVariable()
+        {
+            VariableName variables = new VariableName();
+            Assert.That(CheckType(variables, "char", typeof(char)), Is.True);
+        }
+
+        [Test, Order(3)]
+        public void TestSbyteVariable()
+        {
+            VariableName variables = new VariableName();
+            Assert.That(CheckType(variables, "sbyte", typeof(sbyte)), Is.True);
+        }
+
+        private bool CheckType(object obj, string fieldName, Type expectedType)
+        {
+            var field = obj.GetType().GetField(fieldName);
+            if (field == null) return false;
+
+            return field.FieldType == expectedType;
+        }
+    }
+
+}
